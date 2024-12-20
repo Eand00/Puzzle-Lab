32b591c08ec41d5b3f53a60eb31f4ccfbb10ca70
VicioBonura <vicio.bonura@gmail.com>
2024-12-20 09:14:02 +0100

    refactor web components

--- Progetto_frontend/components/footer-component.js
+++ Progetto_frontend/components/footer-component.js
@@ -9,24 +9,27 @@
  * @see README_FRONTEND.md for additional information.
  */
 
-const componentMarkup = `
-<footer class="py-5 text-light bg-black">
-    <div class="container">
-        <div class="logo-footer"><span class="row1">CACCIA</span><span class="row2">SAPERI</span></div>
-        <p class="copyright text-center">&copy; 2024 Puzzle Lab Torino</p>
-    </div>
-
-    <ul class="footer-nav py-5 px-0">
-        <li><a href="#" class="text-light">PAGINA 1</a></li>
-        <li><a href="#" class="text-light">PAGINA 2</a></li>
-        <li><a href="#" class="text-light">PAGINA 3</a></li>
-    </ul>
-</footer>
-`;
-
 class FooterComponent extends HTMLElement {
     constructor() {
         super();
+        this.template = document.createElement('template');
+        this.template.innerHTML = `
+            <footer class="py-5 text-light bg-black">
+                <div class="container">
+                    <div class="logo-footer">
+                        <span class="row1">CACCIA</span>
+                        <span class="row2">SAPERI</span>
+                    </div>
+                    <p class="copyright text-center">&copy; 2024 Puzzle Lab Torino</p>
+                </div>
+
+                <ul class="footer-nav py-5 px-0">
+                    <li><a href="#" class="text-light">PAGINA 1</a></li>
+                    <li><a href="#" class="text-light">PAGINA 2</a></li>
+                    <li><a href="#" class="text-light">PAGINA 3</a></li>
+                </ul>
+            </footer>
+        `;
     }
 
     /**
@@ -34,14 +37,9 @@ class FooterComponent extends HTMLElement {
      * It is used to initialize the component.
      */
     connectedCallback() {
-        this.render();
-    }
-
-    /**
-     * This method is used to render the component.
-     */
-    render() {
-        this.innerHTML = componentMarkup;
+        const content = this.template.content.cloneNode(true);
+        this.appendChild(content);
     }
 }
+
 customElements.define("footer-component", FooterComponent);
