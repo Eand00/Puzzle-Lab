5dbe014e1c8cf254547c52a5fd84dfc0d41442e5
Eand00 <79508332+Eand00@users.noreply.github.com>
2024-12-22 22:18:10 +0100

    Modificato tipo di campo testo in text

--- Progetto_backend/src/main/java/com/puzzle_lab/entities/Informazione.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/entities/Informazione.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name="informazioni")
 public class Informazione extends Richiesta{
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "TEXT")
 	private String testo;
 }
