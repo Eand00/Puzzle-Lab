cf57fa2cc43db90ac2c774b46ccb4faf426fe563
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-04 17:25:49 +0100

    Pulizia codice

--- Progetto_backend/src/main/java/com/puzzle_lab/controllers/RichiestaController.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/controllers/RichiestaController.java
@@ -1,18 +1,24 @@
 package com.puzzle_lab.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.puzzle_lab.entities.Informazione;
 import com.puzzle_lab.entities.Prenotazione;
 import com.puzzle_lab.services.InformazioneService;
 import com.puzzle_lab.services.PrenotazioneService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Richiesta", description = "Gestione delle richieste di informazioni e prenotazioni")
 @RestController
