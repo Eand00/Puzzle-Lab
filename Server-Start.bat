bd663b3a45eb48490f264d1b1dc55b39a857d828
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-07 16:56:10 +0100

    bat

---
+++ Server-Start.bat
@@ -0,0 +1,11 @@
+SET "BASE_DIR=%~dp0"
+
+cd /d "%BASE_DIR%target"
+
+IF NOT EXIST "cascina-caccia-0.0.1-SNAPSHOT.jar" (
+    echo ERROR: JAR file not found in the target directory!
+    pause
+    exit /b 1
+)
+
+java -jar cascina-caccia-0.0.1-SNAPSHOT.jar
