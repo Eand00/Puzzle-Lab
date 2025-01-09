26d03d3f15569d214b19781793b8c261f69bbfd0
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-09 21:53:38 +0100

    Update RichiestaService.java

--- Progetto_backend/src/main/java/com/puzzle_lab/services/RichiestaService.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/services/RichiestaService.java
@@ -51,7 +51,7 @@ public class RichiestaService {
         if (nome == null || nome.trim().isEmpty()) {
             throw new IllegalArgumentException("Il nome/cognome non può essere vuoto.");
         }
-        if (!nome.matches("^[a-zA-Z\\s]+$")) {
+        if (!nome.matches("^[a-zA-ZÀ-ù'\\s]+$")) {
             throw new IllegalArgumentException("Il nome/cognome può contenere solo lettere e spazi.");
         }
     }
