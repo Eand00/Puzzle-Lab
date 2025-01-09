21be22482c296c35dd806a0fdbb91096f4358244
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-09 20:33:40 +0100

    Abilitato la schedulazione

--- Progetto_backend/src/main/java/com/puzzle_lab/ProgettoBackendApplication.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/ProgettoBackendApplication.java
@@ -2,8 +2,10 @@ package com.puzzle_lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProgettoBackendApplication {
 
 	public static void main(String[] args) {
