c140808b4547619871970ed5bfd72064c80f72e7
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-09 20:34:24 +0100

    Creazione della querry custom cancellazione

--- Progetto_backend/src/main/java/com/puzzle_lab/repos/RichiestaDAO.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/repos/RichiestaDAO.java
@@ -1,15 +1,27 @@
 package com.puzzle_lab.repos;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.puzzle_lab.entities.Richiesta;
+
+import jakarta.transaction.Transactional;
 
 public interface RichiestaDAO extends JpaRepository<Richiesta,Long> {
 
 	Optional<Richiesta> findByEmail(String email);
 
 	boolean existsByEmail(String email);
+	
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM Richiesta u WHERE u.dataPrevistaCancellazione < :currentTime")
+	void deleteByDeleteScheduledAtBefore(@Param("currentTime") LocalDateTime currentTime);
+
 
 }
