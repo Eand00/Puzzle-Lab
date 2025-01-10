17872a39fce08921460a9bdd3c71502f99bf9173
Samuele Sicura <samuele.sicura@edu.itspiemonte.it>
2025-01-10 01:43:59 +0100

    Update RichiestaDAO.java
    aggiunta metodi per filtrare con query custom

--- Progetto_backend/src/main/java/com/puzzle_lab/repos/RichiestaDAO.java
+++ Progetto_backend/src/main/java/com/puzzle_lab/repos/RichiestaDAO.java
@@ -12,22 +12,23 @@ import com.puzzle_lab.entities.Richiesta;
 import com.puzzle_lab.entities.Status;
 
 public interface RichiestaDAO extends JpaRepository<Richiesta,Long> {
-
-	Optional<Richiesta> findByEmail(String email);
 	
-    //@Query("SELECT r FROM Richiesta r WHERE r.status = :status")
     List<Richiesta> findByStatus(Status status);
     List<Richiesta> findByStatusNot(Status status);
     
-	
 	Optional<Richiesta> findByDataCreazione(LocalDateTime data);
-
-	boolean existsByEmail(String email);
-
+	
 	@Query("SELECT r FROM Richiesta r WHERE r.nome LIKE CONCAT('%', :nome, '%')")
 	Optional<List<Richiesta>> findByNome(@Param("nome")String nome);
 	
-	Optional<Richiesta> findByNomeContainingIgnoreCase(String nome);
-
+	@Query("SELECT r FROM Richiesta r WHERE r.cognome LIKE CONCAT('%', :cognome, '%')")
+	Optional<List<Richiesta>> findByCognome(@Param("cognome")String cognome);
 	
+	@Query("SELECT r FROM Richiesta r WHERE r.email LIKE CONCAT('%', :email, '%')")
+	Optional<List<Richiesta>> findByEmail(@Param("email")String email);
+	
+	@Query("SELECT r FROM Richiesta r WHERE r.organizzazione LIKE CONCAT('%', :organizzazione, '%')")
+	Optional<List<Richiesta>> findByOrganizzazione(@Param("organizzazione")String organizzazione);
+
+	boolean existsByEmail(String email);
 }
