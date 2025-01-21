ab985108c5a2fcdbcb02e3961a121d37a6a259d8
Eand00 <79508332+Eand00@users.noreply.github.com>
2025-01-21 15:35:02 +0100

    178 implementazione logintoken backoffice (#182)
    * Implementazione del sistema di token al login
    
    * Rimosso codice di prova

---
+++ backoffice/assets/js/services/auth.service.js
@@ -0,0 +1,40 @@
+import { apiClient } from '../utils/api-client.js';
+import { API_BASE_URL } from '../config/config.js';
+
+export async function login(username, password) {
+    const url = `${API_BASE_URL}/login`;
+    const credentials = {
+        username: username,
+        password: password,
+    };
+
+    try {
+        const response = await apiClient(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa(`${username}:${password}`),
+                'Content-Type': 'application/json'
+            },
+        });
+
+        if (typeof response === 'string') {
+            throw new Error(response);
+        }
+
+        if (response.token) {
+            localStorage.setItem('jwtToken', response.token);
+            return response.token;
+        } else {
+            throw new Error('No token received from server.');
+        }
+    } catch (error) {
+        const toast = document.querySelector('toast-component');
+        toast.showToast('error', 'Errore di login', error.message);
+        throw error;
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('jwtToken');
+    window.location.href = 'login.html';
+}
